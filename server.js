d1e3ffa45763c5d334a4f4f5035922c077a01641
Alejandro Cortez <acortez.dev@gmail.com>
2020-05-30 19:08:52 -0500

    Modified server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/', express.static(path.join('src', 'public')));
+app.use(express.static(path.join('src', 'public')));
 
 app.post('/subscribe', async (req, res) => {
   console.log(req.body)
